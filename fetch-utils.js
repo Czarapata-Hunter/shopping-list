8d2ef28496c1ce4ecdac72f6f23611d8fc37ee58
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-29 08:54:19 -0700

    Before Step A: Some HTML and CSS

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://nudnldbscesultvvymuw.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51ZG5sZGJzY2VzdWx0dnZ5bXV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQyOTQ3NTIsImV4cCI6MTk3OTg3MDc1Mn0.gHh3DxZMH5wFFi-4mm7jCtViGY1Fuojkl0Hr4h6FIhY';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
